92ba8aaecf4426302486e3112c82c1653a083358
KarrLozz <KarrLozz@KarrLozz>
2013-06-20 01:03:25 -0600

    SwingWorker

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WorkerFrame wf = new WorkerFrame();
+		wf.setVisible(true);
+	}
+
+}
